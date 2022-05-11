950ddc78233c4b48b1ab85e0e61da3245e58377d
WindzoR <windzr67@gmail.com>
2022-05-11 21:44:06 +0300

    Spring mock tests

--- src/test/java/ru/bodins/forum/controller/PostControlTest.java
+++ src/test/java/ru/bodins/forum/controller/PostControlTest.java
@@ -1,6 +1,7 @@
 package ru.bodins.forum.controller;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -10,14 +11,20 @@ import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 import ru.bodins.forum.Main;
+import ru.bodins.forum.model.Author;
+import ru.bodins.forum.model.Comment;
 import ru.bodins.forum.model.Post;
 import ru.bodins.forum.service.PostService;
 
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
 import java.util.Date;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.mockito.Mockito.verify;
 
 @SpringBootTest(classes = Main.class)
 @AutoConfigureMockMvc
@@ -41,7 +48,7 @@ public class PostControlTest {
 
     @Test
     @WithMockUser
-    public void updatePost() throws Exception {
+    public void whenUpdatePost() throws Exception {
         Post post = Post.of("name", "description", new Date());
         Mockito.when(service.findPostById(Mockito.anyInt())).thenReturn(post);
         this.mockMvc.perform(get("/update?id=1"))
@@ -57,5 +64,39 @@ public class PostControlTest {
                 .andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(view().name("post/post"));
+    }
+
+    @Test
+    @WithMockUser
+    public void whenSaveNewPost() throws Exception {
+        this.mockMvc.perform(post("/save")
+                .param("name", "Servlet API")
+                .param("description", "Обсуждение сервлетов"))
+                .andDo(print())
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/index"));
+        ArgumentCaptor<Post> argument = ArgumentCaptor.forClass(Post.class);
+        verify(service).savePost(argument.capture());
+        assertThat(argument.getValue().getName(), is("Servlet API"));
+        assertThat(argument.getValue().getDescription(), is("Обсуждение сервлетов"));
+    }
+
+    @Test
+    @WithMockUser
+    public void whenSaveNewComment() throws Exception {
+        Author author = Author.of(1, "Ivan");
+        Post post = Post.of("Servlet API", "Обсуждение сервлетов", new Date());
+        Mockito.when(service.findAuthorById(Mockito.anyInt())).thenReturn(author);
+        Mockito.when(service.findPostById(Mockito.anyInt())).thenReturn(post);
+        this.mockMvc.perform(post("/save/comment")
+                .param("post_id", "1")
+                .param("desc", "Сервлеты - отстой! SpringBoot - forever!"))
+                .andDo(print())
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/view?id=1"));
+        ArgumentCaptor<Comment> argument = ArgumentCaptor.forClass(Comment.class);
+        verify(service).saveComment(argument.capture());
+        assertThat(argument.getValue().getComment(), is("Сервлеты - отстой! SpringBoot - forever!"));
+        assertThat(argument.getValue().getAuthor(), is(author));
     }
 }
--- src/test/java/ru/bodins/forum/controller/RegControlTest.java
+++ src/test/java/ru/bodins/forum/controller/RegControlTest.java
@@ -1,16 +1,28 @@
 package ru.bodins.forum.controller;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 import ru.bodins.forum.Main;
+import ru.bodins.forum.dao.UserRepository;
+import ru.bodins.forum.model.User;
 
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+import static org.junit.Assert.assertTrue;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.mockito.Mockito.verify;
 
 @SpringBootTest(classes = Main.class)
 @AutoConfigureMockMvc
@@ -20,9 +32,39 @@ public class RegControlTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @MockBean
+    private UserRepository users;
+
     @Test
     public void returnRegPage() throws Exception {
         this.mockMvc.perform(get("/reg"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(view().name("reg"));
+    }
+
+    @Test
+    public void whenRegisterNewUser() throws Exception {
+        Mockito.when(users.findDistinctByUsernameLike(Mockito.anyString())).thenReturn(null);
+        this.mockMvc.perform(post("/reg")
+                        .param("username", "test")
+                        .param("password", "123"))
+                .andDo(print())
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/login"));
+        ArgumentCaptor<User> argument = ArgumentCaptor.forClass(User.class);
+        verify(users).save(argument.capture());
+        System.out.println(argument.getAllValues());
+        assertThat(argument.getValue().getUsername(), is("test"));
+        assertTrue(new BCryptPasswordEncoder().matches("123", argument.getValue().getPassword()));
+    }
+
+    @Test
+    public void whenTryRegisterExistUser() throws Exception {
+        Mockito.when(users.findDistinctByUsernameLike(Mockito.anyString())).thenReturn(User.of("root"));
+        this.mockMvc.perform(post("/reg")
+                        .param("username", "root")
+                        .param("password", "123"))
                 .andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(view().name("reg"));
